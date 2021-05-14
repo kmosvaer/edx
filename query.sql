0629fc2f720a725c6003c97314d89eda548bf907
kmosvaer <84209779+kmosvaer@users.noreply.github.com>
2021-05-15 01:28:28 +0200

    Add files via upload

--- query.sql
+++ query.sql
@@ -1,3 +1,6 @@
-SELECT product_code, product_name, list_price, discount_percent
-FROM products
-ORDER BY list_price DESC
+SELECT first_name, 
+last_name, 
+CONCAT(last_name, ', ', first_name) AS full_name
+FROM customers
+WHERE last_name BETWEEN 'M' AND 'Z'
+ORDER BY last_name ASC
