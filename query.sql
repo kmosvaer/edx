789b5c3767cba458797e2c9924c97f3b50ec9e08
kmosvaer <84209779+kmosvaer@users.noreply.github.com>
2021-05-15 17:50:58 +0200

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,6 +1,6 @@
-SELECT first_name, 
-last_name, 
-CONCAT(last_name, ', ', first_name) AS full_name
-FROM customers
-WHERE last_name BETWEEN 'M' AND 'Z'
-ORDER BY last_name ASC
+SELECT product_name, list_price, discount_percent,
+ROUND(list_price * (discount_percent / 100), 2) AS discount_amount,
+ROUND((list_price - (list_price * (discount_percent / 100))), 2) AS discount_price
+FROM products
+ORDER BY discount_price DESC
+LIMIT 5
