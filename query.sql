6aa6bf8ce3f1690223c24ba982366b7cb5cc9dc5
kmosvaer <84209779+kmosvaer@users.noreply.github.com>
2021-05-15 18:29:52 +0200

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,4 +1,3 @@
-SELECT 100 AS price, 
-0.07 AS tax_rate,
-100 * 0.07 AS tax_amount,
-100 + (100 * 0.07) AS total
+SELECT product_code, product_name, list_price, discount_percent
+FROM products
+ORDER BY list_price DESC
