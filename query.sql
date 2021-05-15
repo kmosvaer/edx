170cd52f4f86fa10704b38a648fb1d2fc2c4d3e1
kmosvaer <84209779+kmosvaer@users.noreply.github.com>
2021-05-15 17:59:16 +0200

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,6 +1,8 @@
-SELECT product_name, list_price, discount_percent,
-ROUND(list_price * (discount_percent / 100), 2) AS discount_amount,
-ROUND((list_price - (list_price * (discount_percent / 100))), 2) AS discount_price
-FROM products
-ORDER BY discount_price DESC
-LIMIT 5
+SELECT item_id,
+item_price, discount_amount, quantity,
+item_price * quantity AS price_total,
+discount_amount * quantity AS discount_total,
+(item_price - discount_amount) * quantity AS item_total
+FROM order_items
+WHERE (item_price - discount_amount) * quantity > 500
+ORDER BY item_total DESC
