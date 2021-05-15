25e41c4f3d0965b48918fbd374e874b6559385f4
kmosvaer <84209779+kmosvaer@users.noreply.github.com>
2021-05-15 18:03:29 +0200

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,8 +1,4 @@
-SELECT item_id,
-item_price, discount_amount, quantity,
-item_price * quantity AS price_total,
-discount_amount * quantity AS discount_total,
-(item_price - discount_amount) * quantity AS item_total
-FROM order_items
-WHERE (item_price - discount_amount) * quantity > 500
-ORDER BY item_total DESC
+SELECT order_id, order_date, ship_date
+FROM orders
+WHERE ship_date = null
+ORDER BY order_id DESC
