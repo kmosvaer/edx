8affe46c43f02d2dc3f205531a26dde481ad09fd
kmosvaer <84209779+kmosvaer@users.noreply.github.com>
2021-05-26 19:58:37 +0200

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,8 +1,4 @@
-SELECT 'SHIPPED' as ship_status, order_id, order_date
-	FROM orders
-	WHERE ship_date IS NOT NULL
-UNION 
-SELECT 'NOT SHIPPED' AS ship_status, order_id, order_date
-	FROM orders
-    WHERE ship_date IS NULL
-ORDER BY order_date
+INSERT INTO categories
+(category_name)
+VALUES
+('Brass') 
